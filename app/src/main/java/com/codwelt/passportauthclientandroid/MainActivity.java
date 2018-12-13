57b2a940de683b5df9d54a75467b07c8dc7c1490
FuriosoJack <iam@furiosojack.com>
2018-12-12 22:36:24 -0500

    Se añade proyecti inicial

---
+++ app/src/main/java/com/codwelt/passportauthclientandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.codwelt.passportauthclientandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Dotenv dot = Dotenv.configure().directory("/assets").filename("env").load();
+        String clien = dot.get("CLIENT_ID");
+        Log.d("CLIEN", clien);
+    }
+}
